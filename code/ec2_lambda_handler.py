c11df880413cc0a48a6a66f28dc922d02eb25212
Cristian Sandru <cristian@binovate.com>
2022-04-14 19:04:30 +0300

    fixed filtering ec2 instances

--- code/ec2_lambda_handler.py
+++ code/ec2_lambda_handler.py
@@ -15,9 +15,11 @@ ec2 = boto3.client('ec2', region_name=region)
 
 
 def _get_instances_by_tag(tag_name: str, tag_value: str):
+    # Query EC2 Instances base on a tag name & value
+    # Returns list of found EC2 Instances based on filters
     return ec2.describe_instances(Filters=[
         {
-            'Name': f'{tag_name}',
+            'Name': f'tag:{tag_name}',
             'Values': [
                 f'{tag_value}',
             ]
@@ -28,23 +30,32 @@ def _get_instances_by_tag(tag_name: str, tag_value: str):
 def _get_instance_ids(tag_name: str, tag_value: str):
     response = _get_instances_by_tag(tag_name, tag_value)
     instance_ids = []
-    for reservation in response["Reservations"]:
-        for instance in reservation["Instances"]:
-            instance_ids.append(instance["InstanceId"])
+    if len(response["Reservations"]):
+        logger.info("Matching instance were found. Parsing response to extract Instance Ids")
+        # Parsing the describe instances response in order to extract EC2 Instance Ids
+        for reservation in response["Reservations"]:
+            for instance in reservation["Instances"]:
+                instance_ids.append(instance["InstanceId"])
     return instance_ids
 
 
 def stop(event, context):
     tag_name = os.getenv("STOP_TAG_NAME")
     tag_value = os.getenv("STOP_TAG_VALUE")
-    instances = _get_instance_ids(tag_name, tag_value)
-    ec2.stop_instances(InstanceIds=instances)
-    logger.info('stopped instances: ' + str(instances))
+    instance_ids = _get_instance_ids(tag_name, tag_value)
+    if instance_ids:
+        ec2.stop_instances(InstanceIds=instance_ids)
+        logger.info('Successfully stopped instances: ' + str(instance_ids))
+    else:
+        logger.info(f'No matching Instances were found using tag name: {tag_name} and value: {tag_value}')
 
 
 def start(event, context):
     tag_name = os.getenv("START_TAG_NAME")
     tag_value = os.getenv("START_TAG_VALUE")
-    instances = _get_instance_ids(tag_name, tag_value)
-    ec2.start_instances(InstanceIds=instances)
-    logger.info('started  instances: ' + str(instances))
+    instance_ids = _get_instance_ids(tag_name, tag_value)
+    if instance_ids:
+        ec2.start_instances(InstanceIds=instance_ids)
+        logger.info('Successfully started instances: ' + str(instance_ids))
+    else:
+        logger.info(f'No matching Instances were found using tag name: {tag_name} and value: {tag_value}')
