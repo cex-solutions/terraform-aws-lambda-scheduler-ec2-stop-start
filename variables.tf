be28b6bf986fbc221233976cad6f2dabbcfcea09
Cristian Sandru <cristian@binovate.com>
2022-04-15 13:07:59 +0300

    added default values for variables & lambda zip

--- variables.tf
+++ variables.tf
@@ -10,6 +10,7 @@ variable "region" {
 variable "prefix_name" {
   type        = string
   description = "Prefix name used in order to avoid multiple regions deployment issues"
+  default     = null
 }
 
 
@@ -22,15 +23,18 @@ variable "start_ec2_enabled" {
 variable "start_ec2_schedule_expression" {
   type        = string
   description = "Represents the cron expression used to Start EC2 Instance"
+  default     = "cron(30 6 ? * 2-6 *)"
 }
 
 variable "start_ec2_tag_name" {
   type        = string
   description = "Represents tag key/name of EC2s that needs to be started"
+  default     = "Auto-Start"
 }
 variable "start_ec2_tag_value" {
   type        = string
   description = "Represents tag value of EC2s that needs to be started"
+  default     = "true"
 }
 
 
@@ -44,14 +48,17 @@ variable "stop_ec2_enabled" {
 variable "stop_ec2_schedule_expression" {
   type        = string
   description = "Represents the cron expression used to Stop EC2 Instance"
+  default     = "cron(0 19 ? * 2-6 *)"
 }
 
 variable "stop_ec2_tag_name" {
   type        = string
   description = "Represents tag key/name of EC2s that needs to be stopped"
+  default     = "Auto-Stop"
 }
 
 variable "stop_ec2_tag_value" {
   type        = string
   description = "Represents tag value of EC2s that needs to be stopped"
+  default     = "true"
 }
